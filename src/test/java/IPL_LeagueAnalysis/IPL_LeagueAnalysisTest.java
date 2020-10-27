144fa5cfef1c6a54260cbaddc1e48be95aa0a3c8
ijounjalkar <71863485+ijounjalkar@users.noreply.github.com>
2020-10-28 00:07:12 +0530

    Refactor_UC1:Sorting Top Batting Averages
    "Refactor_UC1:Sorting Top Batting Averages

--- src/test/java/IPL_LeagueAnalysis/IPL_LeagueAnalysisTest.java
+++ src/test/java/IPL_LeagueAnalysis/IPL_LeagueAnalysisTest.java
@@ -2,6 +2,8 @@ package IPL_LeagueAnalysis;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class IPL_LeagueAnalysisTest {
@@ -26,6 +28,13 @@ public class IPL_LeagueAnalysisTest {
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	@Test
+	public void givenCSVFileOfRuns_IfMatchTopBattingAvg_ReturnsThePlayer() throws IOException, CSVBuilderExecption {
+		IPL_LeagueAnalysis test = new IPL_LeagueAnalysis();
+		test.loadMostRunsCSVFile(FileName_MostRuns);
+		double result = test.getTopBattingAvg();
+		assertEquals(83.2, result, 0.0);
 	}
 
 }
