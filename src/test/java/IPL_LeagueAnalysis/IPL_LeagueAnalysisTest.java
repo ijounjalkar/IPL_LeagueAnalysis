ff775f26fc858a1c7515fe7c108b497c7dcc1819
ijounjalkar <jounjalkarisha@gmail.com>
2020-10-27 23:50:34 +0530

    Loading CSV files

---
+++ src/test/java/IPL_LeagueAnalysis/IPL_LeagueAnalysisTest.java
@@ -0,0 +1,31 @@
+package IPL_LeagueAnalysis;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class IPL_LeagueAnalysisTest {
+	String FileName_MostRuns = "C:\\Users\\ADMIN\\eclipse-workspace\\IPL_LeagueAnalysis\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+	String FileName_MostWickets = "C:\\Users\\ADMIN\\eclipse-workspace\\IPL_LeagueAnalysis\\WP DP Data_02 IPL2019FactsheetMostWkts.csv";
+
+	@Test
+	public void givenCSVFileOfRuns_ReturnsNumOfRecord() {
+		IPL_LeagueAnalysis test = new IPL_LeagueAnalysis();
+		try {
+			assertEquals(101, test.loadMostRunsCSVFile(FileName_MostRuns));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenCSVFileOfWickets_ReturnsNumOfRecord() {
+		IPL_LeagueAnalysis test = new IPL_LeagueAnalysis();
+		try {
+			assertEquals(99, test.loadMostWicketsCSVFile(FileName_MostWickets));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
